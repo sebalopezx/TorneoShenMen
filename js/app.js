c5b7e82d9ad801c9d25f323c3578b1cc56483b86
sebalopezx <se.lopezn@gmail.com>
2026-03-26 23:50:22 -0300

    conexion con google sheets

--- js/app.js
+++ js/app.js
@@ -1,5 +1,54 @@
-const API = "TU_GOOGLE_APPS_SCRIPT_URL";
+const API = "https://script.google.com/macros/s/AKfycbzfCXuObPNkFwoj3g3AYPj8rmpkxPJQ2bWS3Mkm1-uBqQNaSJncCxxYOUd1Go1HoZozOQ/exec";
 
+// OBTENER DATA
+
+function cargarCategorias() {
+  fetch(API + "?action=getCategorias")
+    .then(res => res.json())
+    .then(data => {
+      const select = document.getElementById("categoria");
+
+      select.innerHTML = "<option value=''>Seleccionar</option>";
+
+      data.forEach(c => {
+        const option = document.createElement("option");
+        option.value = c.nombre;
+        option.textContent = c.nombre;
+        select.appendChild(option);
+      });
+    });
+}
+
+let competidoresGlobal = [];
+
+function cargarCompetidores() {
+  fetch(API + "?action=getCompetidores")
+    .then(res => res.json())
+    .then(data => {
+      competidoresGlobal = data;
+    });
+}
+
+document.getElementById("categoria").addEventListener("change", function () {
+  const categoria = this.value;
+  const select = document.getElementById("competidor");
+
+  select.innerHTML = "<option value=''>Seleccionar</option>";
+
+  const filtrados = competidoresGlobal.filter(c => c.categoria === categoria);
+
+  filtrados.forEach(c => {
+    const option = document.createElement("option");
+    option.value = c.codigo;
+    option.textContent = c.nombre;
+    select.appendChild(option);
+  });
+});
+
+window.onload = () => {
+  cargarCategorias();
+  cargarCompetidores();
+};
 // ---------------- JUDGE ----------------
 
 function sendScore(score) {
@@ -34,4 +83,10 @@ function loadDisplay() {
         scoresDiv.appendChild(p);
       });
     });
+}
+
+function obtenerData() {
+    fetch(API + "?action=getScores&tatami=1")
+    .then(res => res.json())
+    .then(data => console.log(data));
 }
